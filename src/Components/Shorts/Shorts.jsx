9d659c5016d03d647a12d9aa7d39493227aad56c
enteecaay <kietnguyen060204@gmail.com>
2025-01-15 02:00:11 +0700

    Remove unused video imports from Shorts component

--- src/Components/Shorts/Shorts.jsx
+++ src/Components/Shorts/Shorts.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import "./Shorts.css";
 
 import short1 from "../../assets/short1.mp4";
-import short2 from "../../assets/short2.mp4";
-import short3 from "../../assets/short3.mp4";
 
 const Shorts = () => {
   const [videos, setVideos] = useState([]);
@@ -15,13 +13,6 @@ const Shorts = () => {
         src: short1,
         title: "Short Video 1",
         channelName: "MartiniAsphalt",
-      },
-      { id: 2, src: short2, title: "Short Video 2", channelName: "Damitz2k" },
-      {
-        id: 3,
-        src: short3,
-        title: "Short Video 3",
-        channelName: "NoCopyrightSounds",
       },
     ]);
   }, []);
