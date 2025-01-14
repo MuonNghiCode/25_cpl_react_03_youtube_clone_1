18f74abc26d0ba486c6955bcf93a30c2a209960e
danh-pham235 <phamdanh0305@gmail.com>
2025-01-14 23:16:07 +0700

    update short video

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,438 @@
+import React from "react";
+import "./Feed.css";
+import thumbnail1 from "../../assets/thumbnail1.png";
+import thumbnail2 from "../../assets/thumbnail2.png";
+import thumbnail3 from "../../assets/thumbnail3.png";
+import thumbnail4 from "../../assets/thumbnail4.png";
+import thumbnail5 from "../../assets/thumbnail5.png";
+import thumbnail6 from "../../assets/thumbnail6.png";
+import thumbnail7 from "../../assets/thumbnail7.png";
+import thumbnail8 from "../../assets/thumbnail8.png";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+const Feed = () => {
+  return (
+    <div className="feed">
+      <Link to={`video/20/4521`} className="card">
+        <img src={thumbnail1} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </Link>
+      <div className="card">
+        <img src={thumbnail2} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail3} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail4} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail5} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail6} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail7} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail8} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <Link to={`video/20/4521`} className="card">
+        <img src={thumbnail1} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </Link>
+      <div className="card">
+        <img src={thumbnail2} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail3} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail4} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail5} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail6} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail7} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail8} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <Link to={`video/20/4521`} className="card">
+        <img src={thumbnail1} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </Link>
+      <div className="card">
+        <img src={thumbnail2} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail3} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail4} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail5} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail6} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail7} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail8} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <Link to={`video/20/4521`} className="card">
+        <img src={thumbnail1} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </Link>
+      <div className="card">
+        <img src={thumbnail2} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail3} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail4} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail5} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail6} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail7} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img src={thumbnail8} alt="thumbnail1" />
+        <div className="card-content">
+          <div className="card-header">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <h2>Video Title</h2>
+          </div>
+          <div className="card-content-text">
+            <h3>Channel Name</h3>
+            <p>Video Description (total views + upload date)</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/youtube-logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faBell,
+  faMicrophone,
+  faPlus,
+  faSearch,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ setSidebar }) => {
+  return (
+    <nav className="flex-div">
+      <div className="nav-left flex-div">
+        <button className="menu-btn">
+          <FontAwesomeIcon
+            icon={faBars}
+            className="menu-icon"
+            onClick={() =>
+              setSidebar((prev) => (prev === false ? true : false))
+            }
+          />
+        </button>
+        <Link to="/">
+          <img className="logo" src={logo} alt="logo" />
+        </Link>
+      </div>
+      <div className="nav-middle flex-div">
+        <div className="search-box flex-div">
+          <input type="text" placeholder="Search" />
+          <div className="icon">
+            <FontAwesomeIcon className="search-icon" icon={faSearch} />
+          </div>
+        </div>
+        <FontAwesomeIcon icon={faMicrophone} className="microphone-icon" />
+      </div>
+      <div className="nav-right flex-div">
+        <div className="create-container">
+          <FontAwesomeIcon icon={faPlus} className="plus-icon" />
+          <span>Create</span>
+        </div>
+        <FontAwesomeIcon icon={faBell} className="bell-icon" />
+        <FontAwesomeIcon icon={faUser} className="user-icon" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
